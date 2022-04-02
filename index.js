1f2ec22b966c96a91cc59f97559858aed0e383cb
Mohamed aamiin <engmohammedaamiin.com>
2022-04-02 07:25:05 -0700

    login form with google

---
+++ index.js
@@ -0,0 +1,16 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image".attr("src",profile.getImageUrL()));
+    $(".data").css("display","block");
+    $(".g-signin2").css("display","none");
+  }
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert("you have been sign out successfully");
+      $(".g-signin2").css("display","block");
+      $(".data").css("display","none");
+    });
+}
